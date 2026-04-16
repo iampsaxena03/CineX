e6e7366b5ab8c2ddcc8787b14d84e9c8b880bc07
iampsaxena03 <iampsaxena03@gmail.com>
2026-04-16 15:25:54 +0530

    Fix next-pwa typing error

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,10 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true,
   cacheOnFrontEndNav: true,
+  workboxOptions: {
+    skipWaiting: true,
+  },
 });
 
 const nextConfig: NextConfig = {
